3019ce25dd0b44f8814b04689f40789161bbf7b1
Frank Sun <franksun418@gmail.com>
2020-02-23 16:57:12 +0800

    Update BuildProcessor.cs

--- Assets/Editor/BuildProcessor.cs
+++ Assets/Editor/BuildProcessor.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BuildProcessor.cs : MonoBehaviour
+﻿using UnityEditor;
+using UnityEditor.Callbacks;
+#if UNITY_IOS
+using System.IO;
+using UnityEditor.iOS.Xcode;
+#endif
+ 
+public class BuildProcessor
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   [PostProcessBuild(1)]
+   public static void OnPostprocessBuild(BuildTarget buildTarget, string pathToBuiltProject)
+   {
+#if UNITY_IOS
+      // Get plist
+      string plistPath = pathToBuiltProject + "/Info.plist";
+      PlistDocument plist = new PlistDocument();
+      plist.ReadFromString(File.ReadAllText(plistPath));
+ 
+      // Get root
+      PlistElementDict rootDict = plist.root;
+ 
+      // Set encryption usage boolean
+      string encryptKey = "ITSAppUsesNonExemptEncryption";
+        rootDict.SetBoolean(encryptKey, false);
+ 
+      // remove exit on suspend if it exists.
+      string exitsOnSuspendKey = "UIApplicationExitsOnSuspend";
+      if(rootDict.values.ContainsKey(exitsOnSuspendKey))
+      {
+         rootDict.values.Remove(exitsOnSuspendKey);
+      }
+ 
+      // Write to file
+      File.WriteAllText(plistPath, plist.WriteToString());
+#endif
+   }
 }
